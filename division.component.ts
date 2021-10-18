e38f7da099786c84f474bc9564e105defb12e3d7
StutiHaritaas <92727042+StutiHaritaas@users.noreply.github.com>
2021-10-18 21:26:00 +0530

    Add files via upload

---
+++ division.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core’;
+@Component({
+    selector: 'app-division',
+templateUrl: './division.component.html',
+styleUrls: ['./division.component.css']
+})
+export class DivisionComponent {
+@Input() num1!:string;
+@Input() num2!:string;
+res!:number;
+@Output() sendDivision = new EventEmitter<number>();
+division(first: string, second: string)
+{
+this.res= parseInt(first) / parseInt(second);
+this.sendDivision.emit(this.res);
+}
+}
